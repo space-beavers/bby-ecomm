a69fed1c21e1d0286d1949f771da3afebf7f81a1
Mike Buckley <mikebuckley.canada@gmail.com>
2016-05-22 20:27:34 -0700

    Initial commit

---
+++ sw-precache-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/*'
+  ],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [
+    {
+      // urlPattern: /\/data\/images\/.*/,
+      urlPattern: /\/multimedia.bbycastatic.ca\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/www-ssl.bestbuy.ca\/api\/v2\/json\/search\//,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
+};
